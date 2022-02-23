6680136eb43b22bd7feb535b95c2ba4949d4c5cf
Daniel DeGroff <daniel@fusionauth.io>
2022-02-22 22:45:18 -0700

    add copyright, add notes

--- src/main/java/io/fusionauth/domain/scim/SCIMServiceProviderConfig.java
+++ src/main/java/io/fusionauth/domain/scim/SCIMServiceProviderConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
 package io.fusionauth.domain.scim;
 
 import java.util.ArrayList;
@@ -6,14 +21,18 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * @author Rob Davis
+ */
 public class SCIMServiceProviderConfig extends BaseSCIMResource implements Buildable<SCIMServiceProviderConfig> {
-
   public ArrayList<Map<String, Object>> authenticationSchemes;
 
+  // TODO : SCIM : Can we use Map.of instead of Stream.of(... etc ?
+  // TODO : Don't these need to be configured or set by the SCIM server, should we be initializing them here?
   public Map<String, Object> bulk = Stream.of(new Object[][]{
       {"supported", false},
-      {"maxOperations", 1000},
-      {"maxPayloadSize", 1048576}
+      {"maxOperations", 1_000},
+      {"maxPayloadSize", 104_8576}
   }).collect(Collectors.toMap(data -> (String) data[0], data -> data[1]));
 
   public Map<String, Object> changePassword = Stream.of(new Object[][]{
